888d7497073cf50e1a9dbdeddbff928fe02ec1f1
Alexis Hope <bambishope@gmail.com>
2014-06-11 16:32:11 +1000

    working ajax function

--- dist/vestige-v0.0.0.js
+++ dist/vestige-v0.0.0.js
@@ -190,20 +190,76 @@ v.e({
  * 2 (loaded)
  * 3 (interactive)
  * 4 (complete)
+ * 
+ * Event Summary
+ * readystatechange 
+ * loadstart 
+ * progress 
+ * @TODO abort 
+ * error 
+ * load 
+ * @TODO timeout 
+ * loadend 
+ *
  **/
 v.e({
     
-    ajax: function( options ) {
+    ajax: function( options, callback ) {
         var httpRequest, defaults;
-        if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        
+        
+        if (window.XMLHttpRequest) {
             httpRequest = new XMLHttpRequest();
         } else {
             console.log('Cannot create an XMLHTTP instance');
             return false;
         }
-        httpRequest.onreadystatechange = alertContents;
-        httpRequest.open('GET', url);
-        httpRequest.send();
+        
+        // support short hand, assume options is a URL
+        if( typeof options === 'string' ) {
+            options = {url: options};
+        }
+        
+        // allows success callback to be bundled with options
+        if ( typeof callback === 'function' ) {
+            defaults.success = callback;
+        }
+        
+        defaults = {
+            url: "",
+            REST: "GET",
+            data: null,
+            onerror: v.ajaxError
+        }
+        v.extend( defaults, options );
+
+        
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState == 4) {
+                defaults.success( httpRequest );
+            }
+        };
+        httpRequest.open( defaults.restOpt, defaults.url );
+        
+        // check if POST and data is not empty
+        if( defaults.REST === "POST" && defaults.data !== null ) {
+            // then set content type to JSON
+            httpRequest.setRequestHeader( "Content-Type", "application/json;charset=UTF-8" );
+            httpRequest.send( JSON.stringify( defaults.data ) );
+        } else {
+            // no data, just ping the URL
+            httpRequest.send();
+        }
+        
+    },
+    
+    ajaxError: function( error ) {
+        console.log( 'V AJAX Error: ' + error );
+    },
+    
+    // short hand for post
+    post: function( url, data, callback ) {
+        v.ajax( {url: url, data: data, REST: "POST"}, callback );
     }
 
 });
