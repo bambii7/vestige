fb22570b850472fd028d91caf6efe96dffccd6e5
Alexis Hope <bambishope@gmail.com>
2014-06-11 16:51:45 +1000

    looking at seralize form

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1,6 +1,38 @@
 
 v.e({
 
-    create: v.doc.createElement
+    create: v.doc.createElement,
+    
+    serializeObject = function( Object ) {
+        var o = {};
+        var a = this.serializeArray();
+        v.each(a, function() {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    },
+        
+    $.fn.serializeArray = function() {
+        var result = [], el
+        $([].slice.call(this.get(0).elements)).each(function(){
+        el = $(this)
+        var type = el.attr('type')
+        if (this.nodeName.toLowerCase() != 'fieldset' &&
+        !this.disabled && type != 'submit' && type != 'reset' && type != 'button' &&
+        ((type != 'radio' && type != 'checkbox') || this.checked))
+        result.push({
+        name: el.attr('name'),
+        value: el.val()
+        })
+        })
+        return result
+    }
     
 });
