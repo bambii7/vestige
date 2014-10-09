851a1c26eba9a85b47e5a14593e63053abf5fe3e
Alexis <alexis@cvofhope.com>
2014-10-09 21:53:55 +0200

    restore string

--- tests/string/stringSpec.js
+++ tests/string/stringSpec.js
@@ -1,12 +1,12 @@
 describe( "string", function() {
 
-    xit("should convert a css dashed property into a js camelcase string",function(){
+    it("should convert a css dashed property into a js camelcase string",function(){
         expect( v.str.camelize('font-size') ).toEqual('fontSize');
         expect( v.str.camelize('border-top') ).toEqual('borderTop');
         expect( v.str.camelize('margin-bottom') ).toEqual('marginBottom');
     });
 
-    xit("should covert a js camelcase to a css dashed string",function(){
+    it("should covert a js camelcase to a css dashed string",function(){
         expect( v.str.dasherize('fontSize') ).toEqual('font-size');
         expect( v.str.dasherize('borderTop') ).toEqual('border-top');
         expect( v.str.dasherize('marginBottom') ).toEqual('margin-bottom');
