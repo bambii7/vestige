2337a9ea4fd992c04a0f31a36d44ac53affedcdd
Alexis Hope <alexis.hope@digitaldialogue.com.au>
2016-12-02 16:07:38 +1100

    document vStr class

--- src/string/vString.js
+++ src/string/vString.js
@@ -1,15 +1,29 @@
 (function(v){
 
-    function vStr() {
+    /**
+     * @class vStr
+     * vStr is used as css manipulator helpers. Javascript doesn't support dashed variables
+     * These functions translate css properties like font-size into js FontSize
+     **/
+    var vStr = {};
 
-    }
-
-    // zepto camlize
-    vStr.prototype.camelize = function( str ){
+    /**
+     * Camelcases a dash string
+     * v.str.camelize('something-dark-side') -> 'SomethingDarkSide'
+     * @param string
+     * @return string
+     **/
+    vStr.camelize = function( str ){
         return str.replace(/-+(.)?/g, function(match, chr){ return chr ? chr.toUpperCase() : '' })
     }
 
-    vStr.prototype.dasherize = function(str) {
+    /**
+     * Camelcases a string
+     * v.str.dasherize('somethingDarkSide') -> 'something-dark-side'
+     * @param string
+     * @return string
+     **/
+    vStr.dasherize = function(str) {
         return str.replace(/::/g, '/')
            .replace(/([A-Z]+)([A-Z][a-z])/g, '$1_$2')
            .replace(/([a-z\d])([A-Z])/g, '$1_$2')
@@ -17,6 +31,6 @@
            .toLowerCase()
     }
 
-    v.e({str: vStr.prototype});
+    v.e({str: vStr});
 
 })(v)
