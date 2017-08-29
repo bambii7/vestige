34ca90e810e4ba9e1f4e7ecb7b632c222c2f697e
Alexis Hope <alexis@cvofhope.com>
2017-08-29 10:06:51 +1000

    stylistic update to ajax class for es5

--- src/net/ajax.js
+++ src/net/ajax.js
@@ -22,20 +22,16 @@ v.e({
     ajax: function( options, callback ) {
         let httpRequest, defaults
         if (window.XMLHttpRequest) {
-            httpRequest = new XMLHttpRequest();
+            httpRequest = new XMLHttpRequest()
         } else {
           throw 'Cannot create an XMLHTTP instance'
         }
 
         // support short hand, assume options is a URL
-        if (typeof options === 'string') {
-            options = {url: options};
-        }
+        if (typeof options === 'string') options = {url: options}
 
         // allows success callback to be bundled with options
-        if (typeof callback === 'function') {
-            options.success = callback;
-        }
+        if (typeof callback === 'function') options.success = callback
 
         defaults = {
             url: "",
@@ -43,24 +39,22 @@ v.e({
             data: null,
             onerror: v.ajaxError
         }
-        v.extend( defaults, options );
+        v.extend( defaults, options )
 
 
-        httpRequest.onreadystatechange = function() {
-            if (httpRequest.readyState == 4) {
-                defaults.success( httpRequest );
-            }
-        };
-        httpRequest.open( defaults.REST, defaults.url );
+        httpRequest.onreadystatechange = () => {
+          if (httpRequest.readyState == 4) defaults.success(httpRequest)
+        }
+        httpRequest.open( defaults.REST, defaults.url )
 
         // check if POST and data is not empty
         if( defaults.REST === "POST" && defaults.data !== null ) {
             // then set content type to JSON
-            httpRequest.setRequestHeader( "Content-Type", "application/json;charset=UTF-8" );
-            httpRequest.send( JSON.stringify( defaults.data ) );
+            httpRequest.setRequestHeader( "Content-Type", "application/json;charset=UTF-8" )
+            httpRequest.send( JSON.stringify( defaults.data ) )
         } else {
             // no data, just ping the URL
-            httpRequest.send();
+            httpRequest.send()
         }
 
     },
@@ -70,8 +64,8 @@ v.e({
     },
 
     // short hand for post
-    post: function( url, data, callback ) {
-        v.ajax( {url: url, data: data, REST: "POST"}, callback );
+    post: function(url, data, callback) {
+        v.ajax( {url: url, data: data, REST: "POST"}, callback )
     }
 
-});
+})
